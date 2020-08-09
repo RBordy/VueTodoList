fc0acdd84775905262a70a39dc354b4797e28c51
RBordy <robert.bordynuik@live.com>
2020-08-08 23:09:21 -0400

    Commit 2

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList App 1.0</p>
+    <p>It is part of the Traversy Vue crash course on YouTube</p>
+    <p>https://www.youtube.com/watch?v=Wy9q22isx3U&t=226s</p>
+  </div>
+</template>
